ceadc44825206a12b0053ad70644e40693fea2c9
echo <91651232+exhq@users.noreply.github.com>
2022-12-24 19:53:59 +0330

    AAAAAAAAAAAAAAAAA

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -14,33 +12,14 @@ func queryParamDisplayHandler(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set("Access-Control-Allow-Origin", "*")
 	print(req.FormValue("q"))
 
-	io.WriteString(res, "gay")
+	io.WriteString(res, getshit(req.FormValue("q")))
 
 }
 
 var myClient = &http.Client{Timeout: 10 * time.Second}
 
-func getJson(url string, target interface{}) error {
-	r, err := myClient.Get(url)
-	if err != nil {
-		return err
-	}
-	defer r.Body.Close()
-
-	return json.NewDecoder(r.Body).Decode(target)
-}
-
-type Foo struct {
-	Bar string
-}
-
-func main() {
-	//http.HandleFunc("/youtuber", func(res http.ResponseWriter, req *http.Request) {
-	//	queryParamDisplayHandler(res, req)
-	//})
-	//log.Fatal(http.ListenAndServe(":5000", nil))
-	url := "https://api.song.link/v1-alpha.1/links?url=https://open.spotify.com/track/4OHAGy4roqG6Aog8XrTOez?si=P_OEMTCYR6-84UA8LKxKUQ"
-	fmt.Printf("HTML code of %s ...\n", url)
+func getshit(x string) string {
+	url := x
 	resp, err := http.Get(url)
 	// handle the error if there is one
 	if err != nil {
@@ -54,5 +33,12 @@ func main() {
 		panic(err)
 	}
 	// show the HTML code as a string %s
-	fmt.Printf("%s\n", html)
+	return string(html)
+}
+
+func main() {
+	//http.HandleFunc("/youtuber", func(res http.ResponseWriter, req *http.Request) {
+	//	queryParamDisplayHandler(res, req)
+	//})
+	//log.Fatal(http.ListenAndServe(":5000", nil))
 }
