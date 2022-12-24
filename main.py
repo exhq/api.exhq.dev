77c12f5e8a402e06dae7a7fbb18c8d4e45e4e716
echo <91651232+exhq@users.noreply.github.com>
2022-12-24 18:11:04 +0330

    my code sucks

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
-from bs4 import BeautifulSoup
-import flask
 from youtuber import youtuber
-app = flask.Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-
-    if bool(BeautifulSoup(path, "html.parser").find()):
-        return "kill yourself"
-    else:
-        strpath = str(path).split("/")
-        if len(path) == 0:
-            return "ill add a api list at some point lol"
-        if strpath[0] == "youtuber":
-            x = youtuber(str(path).replace("youtuber/", ""))
-            print(len(strpath))
-            if len(strpath) < 2 or strpath[1] == "":
-                return flask.redirect('https://www.youtube.com/watch?v=YonS9_QJbp8')
-            return x
-        else:
-            return "unknown api: " + path[0]
+@app.route('/youtuber/<link>')
+def show_user_profile(link):
+    x = youtuber(link)
+    return x
 
 
 if __name__ == '__main__':
