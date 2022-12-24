ebd46a9e35eb71057204e68e4bff611cd3b8ca3d
echo <91651232+exhq@users.noreply.github.com>
2022-12-24 19:51:24 +0330

    at this point idk

--- main.go
+++ main.go
@@ -1,33 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"time"
 )
 
 func queryParamDisplayHandler(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(200)
 	res.Header().Set("Access-Control-Allow-Origin", "*")
 	print(req.FormValue("q"))
 
-	io.WriteString(res, getjson(req.FormValue("q")))
+	io.WriteString(res, "gay")
+
 }
 
-func getjson(link string) string {
-	resp, err := http.Get("https://api.song.link/v1-alpha.1/links?url=" + link)
+var myClient = &http.Client{Timeout: 10 * time.Second}
+
+func getJson(url string, target interface{}) error {
+	r, err := myClient.Get(url)
 	if err != nil {
-		fmt.Println("No response from request")
+		return err
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	return string(body)
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
+}
+
+type Foo struct {
+	Bar string
 }
 
 func main() {
-	http.HandleFunc("/youtuber", func(res http.ResponseWriter, req *http.Request) {
-		queryParamDisplayHandler(res, req)
-	})
-	log.Fatal(http.ListenAndServe(":5000", nil))
+	//http.HandleFunc("/youtuber", func(res http.ResponseWriter, req *http.Request) {
+	//	queryParamDisplayHandler(res, req)
+	//})
+	//log.Fatal(http.ListenAndServe(":5000", nil))
+	url := "https://api.song.link/v1-alpha.1/links?url=https://open.spotify.com/track/4OHAGy4roqG6Aog8XrTOez?si=P_OEMTCYR6-84UA8LKxKUQ"
+	fmt.Printf("HTML code of %s ...\n", url)
+	resp, err := http.Get(url)
+	// handle the error if there is one
+	if err != nil {
+		panic(err)
+	}
+	// do this now so it won't be forgotten
+	defer resp.Body.Close()
+	// reads html as a slice of bytes
+	html, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	// show the HTML code as a string %s
+	fmt.Printf("%s\n", html)
 }
